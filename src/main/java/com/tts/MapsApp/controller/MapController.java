c282f35cadc504990ba15cba34409575d5002528
Sherry Xiong <xsherry22@gmail.com>
2019-12-13 09:12:39 -0500

    Deserialize geocoding. Final app

--- src/main/java/com/tts/MapsApp/controller/MapController.java
+++ src/main/java/com/tts/MapsApp/controller/MapController.java
@@ -2,7 +2,9 @@ package com.tts.MapsApp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.tts.MapsApp.service.Location;
 import com.tts.MapsApp.service.MapService;
@@ -14,15 +16,17 @@ public class MapController {
 	@Autowired
 	private MapService mapService;
 	
-	@GetMapping("/home")
-    public String getDefaultMap(Object url) {
-		Location location = new Location();
-		location.setCity("Dallas");
-		location.setState("Texas");
-		mapService.addCoordinates(location);
-		System.out.println(location);
+	@GetMapping("/")
+    public String getDefaultMap(Model model) {
+		model.addAttribute(new Location());
 		return "index.html";
     }
+	@PostMapping("/")
+	public String getMapForLocation(Location location, Model model) {
+	    mapService.addCoordinates(location);
+	    model.addAttribute(location);
+	    return "index.html";
+	}
 }
 	
 
--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -1,8 +1,8 @@
 var map;
-var coords = {lat: 39.9612, lng: -82.9988};
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: coords,
+    	center: coords,
         zoom: 10,
         scrollwheel: false
     });
@@ -17,8 +17,8 @@ function initMap() {
         animation: google.maps.Animation.BOUNCE
     });
     
-    var contentString = '<h2>Columbus, OH</h2> <p>Where your dreams come true.</p>';
-
+    var contentString = '<h2>' + city + ', ' + state + '</h2>';
+    
     var infowindow = new google.maps.InfoWindow({
       content: contentString
     });
